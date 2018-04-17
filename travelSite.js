242f98633a85cd872867116a7ce42b216d58427e
Hunt Applegate <huntapplegate@gmail.com>
2018-04-16 18:48:02 -0600

    first commit

---
+++ travelSite.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+  $("#hidden").hover(function() {
+    $( this ).css("color", "black)");
+  }),
+  function() {
+    $(this).hide();
+  });
+});
+
+console.log("Hello")
+let hello = Hunt
+print(hello)
